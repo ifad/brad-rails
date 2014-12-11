11ee4383cab5f666536127190f54b9db15de55f6
Lleïr Borràs Metje <lleir@llegeix.me>
2014-12-11 11:23:07 +0100

    Improve js readability

--- app/assets/javascripts/brad.js
+++ app/assets/javascripts/brad.js
@@ -1,12 +1,16 @@
 $(function () {
-  $('[data-toggle="brad-popover"]').popover({
-    content: function(){
-      return $($(this).attr("data-target")).find(".content").html()
-    },
-    title: function(){
-      return $($(this).attr("data-target")).find(".title").html()
-    },
-    html: true
-  }).on("click", function(e){ e.preventDefault();})
+  $('[data-toggle="brad-popover"]')
+    .popover({
+      content: function(){
+        return $($(this).attr("data-target")).find(".content").html();
+      },
+      title: function(){
+        return $($(this).attr("data-target")).find(".title").html();
+      },
+      html: true
+    })
+    .on("click", function(e){
+      e.preventDefault();
+    });
 })
 
